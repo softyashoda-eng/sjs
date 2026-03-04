b8458cfd4e497ad35d5b8fdf30ad590b39d1f9aa
softyashoda-eng <softyashoda@gmail.com>
2026-03-04 22:31:47 +0530

    login page index page css and js codes added

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzqGGU2ZZ0lAKtfwd0p9wjTCy4frE6nskymj9rDFI6hYXePOFJCNZMS72D0tq0Vhpu3jA/exec'; // Paste your URL here
+
+document.getElementById('contactForm').addEventListener('submit', e => {
+  e.preventDefault();
+  
+  fetch(scriptURL, { 
+    method: 'POST', 
+    body: new FormData(document.getElementById('contactForm')) 
+  })
+  .then(response => alert("Data saved!"))
+
+  .catch(error => console.error('Error!', error.message));
+  contactForm.reset(); 
+});
+
 
